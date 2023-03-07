b5385b8b0fc77643b243f5b5c7420d0d17f01538
Salman hayat <36339248+salmanhayat1998@users.noreply.github.com>
2023-03-07 09:33:56 +0500

    Update Maps-STL.cpp

--- Maps-STL.cpp
+++ Maps-STL.cpp
@@ -11,55 +11,33 @@ using namespace std;
 
 int main() 
 {
-    int q=0;
-    cin>>q;
-    map<string,int> m;
-    for (int i=0; i<q; i++)
-    {
-        int query;
-        int y; // marks
-        string x; // name 
-        cin>>query; 
-     
-        switch (query)
-        {
- 
-        case 1:
-        cin>>x;
-        cin>>y; 
-         if(m.find(x) == m.end())
-         {
-        m.insert(make_pair(x,y));
-         }
-         else{
-             m[x]+=y; // add in prev marks //
-         }
-        break;
-        case 2:
-        cin>>x;
-        m.erase(x);
-        break;
-        case 3:
-        cin>>x;
-        if(m.find(x) != m.end())
-        {
-        map<string,int>::iterator itr=m.find(x);
-        cout<<itr->second<<endl;   
-        }
-        else{
-            cout<<"0";
-        }
-        break;
-        
-        
-        }
-    }
-
-
+     int q;
+  cin >> q;
+  map <string, int> studentMarks;
+  for (int i = 0; i < q; i++) {
+    int type; 
+    cin >> type;
+    string name;
+    cin >>name;
+  if(type == 1) {
+       int marks; 
+       cin >> marks;
+     if(studentMarks.find(name)!=studentMarks.end()){
+            int updatemarks = studentMarks[name] + marks;
+            studentMarks[name] = updatemarks;
+     }
+     studentMarks.insert(pair<string, int> (name, marks));
+  }
+  if(type == 2) {
+      studentMarks.erase(name);
+  }
+  if (type == 3) {
+     cout << studentMarks[name] << endl;
+  }
+  }
 
 
     return 0;
 }
-
 
 
